66d3593a965f64ceeb17a3d80f343bcba2cd5770
Dann38 <38dunn@gmail.com>
2024-02-22 11:24:15 +0800

    fix example

--- imgDoc/src/img_doc/document/page/extractors/block_extractors/block_classificators/base_block_classificator.py
+++ imgDoc/src/img_doc/document/page/extractors/block_extractors/block_classificators/base_block_classificator.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 Гипотеза в том, что для классификации блока, кроме как информации о блока ничего не нужно
 """
 
-BLOCK_LABEL = ["no_struct", "text", "header",  "list", "table"]
+BLOCK_LABEL = ["no_struct", "text", "header",  "list", "table", "error"]
 
 class BaseBlockClassificator(ABC):
     @abstractmethod
